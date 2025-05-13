874aad9e69a2fc766c20c7a09da0d256e3a2b84c
krishjaiswalx <krishjaiswal941@gmail.com>
2025-05-13 16:12:28 +0530

    bio

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,20 +1,18 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Krish Jaiswal",
   roles: [
     "Full Stack Developer",
-    "Android Developer",
-    "UI/UX Designer",
     "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/krishjaiswalx",
   resume:
     "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+  linkedin: "https://www.linkedin.com/in/krishjaiswalx/",
+  twitter: "https://twitter.com/krishjaiswalx",
+  insta: "https://www.instagram.com/krishjaiswalx/",
+  facebook: "https://www.facebook.com/krishjaiswalx/",
 };
 
 export const skills = [
